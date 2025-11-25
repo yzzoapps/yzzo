4e238e44944b84a74a256361ece99ade39dee683
Lucca Romaniello <37159767+luccaromaniello@users.noreply.github.com>
2025-11-24 23:23:44 -0300

    feat: add tray icon

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,10 +1,20 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+use tauri::{
+    menu::{Menu, MenuItem},
+    tray::TrayIconBuilder,
+};
 use tauri_plugin_clipboard_manager;
 
 fn main() {
     tauri::Builder::default()
         .plugin(tauri_plugin_clipboard_manager::init())
+        .setup(|app| {
+            let tray = TrayIconBuilder::new()
+                .icon(app.default_window_icon().unwrap().clone())
+                .build(app)?;
+            Ok(())
+        })
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
