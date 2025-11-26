0c9132b513d499e8368d6443c82f6b2ef15481a4
Lucca Romaniello <37159767+luccaromaniello@users.noreply.github.com>
2025-11-25 22:38:09 -0300

    feat: add startup on boot

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -4,17 +4,28 @@ use tauri::{
     tray::{MouseButton, MouseButtonState, TrayIconBuilder, TrayIconEvent},
     Manager,
 };
+use tauri_plugin_autostart::MacosLauncher;
+use tauri_plugin_autostart::ManagerExt;
 use tauri_plugin_clipboard_manager;
 use tauri_plugin_positioner::{self, Position, WindowExt};
 
 fn main() {
-    tauri::Builder::default()
+    let mut app = tauri::Builder::default()
+        .plugin(tauri_plugin_autostart::init(
+            MacosLauncher::LaunchAgent,
+            Some(vec!["--tray-only"]),
+        ))
         .plugin(tauri_plugin_clipboard_manager::init())
+        .plugin(tauri_plugin_positioner::init())
         .setup(|app| {
             #[cfg(desktop)]
             {
-                app.handle().plugin(tauri_plugin_positioner::init());
-                let tray = TrayIconBuilder::new()
+                // Get the autostart manager
+                let autostart_manager = app.autolaunch();
+                // Enable autostart
+                let _ = autostart_manager.enable();
+
+                TrayIconBuilder::new()
                     .icon(app.default_window_icon().unwrap().clone())
                     .on_tray_icon_event(|tray, event| {
                         tauri_plugin_positioner::on_tray_event(tray.app_handle(), &event);
@@ -43,6 +54,11 @@ fn main() {
                 Ok(())
             }
         })
-        .run(tauri::generate_context!())
-        .expect("error while running tauri application");
+        .build(tauri::generate_context!())
+        .expect("error building the tauri application");
+
+    #[cfg(target_os = "macos")]
+    app.set_activation_policy(tauri::ActivationPolicy::Accessory);
+
+    app.run(|_app_handle, _event| {});
 }
