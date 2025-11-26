dcd242145d2ba6c370c501a52347653f447034d8
Lucca Romaniello <37159767+luccaromaniello@users.noreply.github.com>
2025-11-25 22:18:59 -0300

    feat: attaches window to tray icon

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,16 +1,47 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
-use tauri::tray::TrayIconBuilder;
+use tauri::{
+    tray::{MouseButton, MouseButtonState, TrayIconBuilder, TrayIconEvent},
+    Manager,
+};
 use tauri_plugin_clipboard_manager;
+use tauri_plugin_positioner::{self, Position, WindowExt};
 
 fn main() {
     tauri::Builder::default()
         .plugin(tauri_plugin_clipboard_manager::init())
         .setup(|app| {
-            let tray = TrayIconBuilder::new()
-                .icon(app.default_window_icon().unwrap().clone())
-                .build(app)?;
-            Ok(())
+            #[cfg(desktop)]
+            {
+                app.handle().plugin(tauri_plugin_positioner::init());
+                let tray = TrayIconBuilder::new()
+                    .icon(app.default_window_icon().unwrap().clone())
+                    .on_tray_icon_event(|tray, event| {
+                        tauri_plugin_positioner::on_tray_event(tray.app_handle(), &event);
+
+                        match event {
+                            TrayIconEvent::Click {
+                                button: MouseButton::Left,
+                                button_state: MouseButtonState::Up,
+                                ..
+                            } => {
+                                println!("left click pressed and released");
+                                let app = tray.app_handle();
+                                if let Some(window) = app.get_webview_window("main") {
+                                    let _ = window.unminimize();
+                                    let _ = window.show();
+                                    let _ = window.move_window(Position::TrayCenter).unwrap();
+                                    let _ = window.set_focus();
+                                }
+                            }
+                            _ => {
+                                println!("unhandled event {event:?}");
+                            }
+                        }
+                    })
+                    .build(app)?;
+                Ok(())
+            }
         })
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
