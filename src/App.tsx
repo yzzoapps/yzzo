891f0cc0a807096ce594985e70f03ce41187fb04
Lucca Romaniello <37159767+luccaromaniello@users.noreply.github.com>
2025-12-05 21:35:05 -0300

    chore: removes unused imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import { useEffect, useState } from "react";
 import reactLogo from "./assets/react.svg";
-import { invoke } from "@tauri-apps/api/core";
 import "./App.css";
 import { writeText, readText } from "@tauri-apps/plugin-clipboard-manager";
 import { getItems, addItem } from "@yzzo/api/tauriApi";
