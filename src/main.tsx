1dc98929b936ec05f19e4390d0fc55e4ceafd213
Lucca Romaniello <37159767+luccaromaniello@users.noreply.github.com>
2025-12-05 23:21:35 -0300

    fix: i18n

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "@tanstack/react-router";
 import { router } from "@yzzo/routes";
+import "@yzzo/i18n";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
