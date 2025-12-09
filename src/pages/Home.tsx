a6b389f77fcc1136dfb1984550f6bfe833dc9bf7
Lucca Romaniello <37159767+luccaromaniello@users.noreply.github.com>
2025-12-09 18:31:22 -0300

    feat: adds reading items to new home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,29 @@
 import { Link } from "@tanstack/react-router";
+import { getItems } from "@yzzo/api/tauriApi";
+import { useClipboardEventWatcher } from "@yzzo/hooks/clipboardWatcher";
+import { Item } from "@yzzo/models/Item";
+import { useEffect, useState } from "react";
 
 const Home = () => {
+  const [items, setItems] = useState<Item[]>([]);
+  const clipboardText = useClipboardEventWatcher();
+
+  useEffect(() => {
+    (async () => {
+      const existingItems = await getItems();
+      setItems(existingItems);
+    })();
+  }, []);
+
+  useEffect(() => {
+    (async () => {
+      const updatedItems = await getItems();
+      setItems(updatedItems);
+    })();
+  }, [clipboardText]);
+
   return (
-    <div className="h-full">
+    <div className="flex flex-col h-screen">
       <nav className="flex flex-row items-center justify-between h-16 px-4 py-10">
         <Link to="/" className="h-10">
           <img src="logo.svg" alt="Logo" className="h-full w-auto" />
@@ -11,7 +32,15 @@ const Home = () => {
           <Link to="/settings">S</Link>
         </div>
       </nav>
-      Home
+      <div className="flex-1 overflow-y-auto">
+        <ul className="space-y-2 w-full">
+          {items.map((item, idx) => (
+            <li key={idx} className="p-2 rounded shadow w-full wrap-break-word">
+              {item.content}
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 };
