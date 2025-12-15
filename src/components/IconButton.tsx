053889821d971605529f1dffae8b7af7626fccd1
Lucca Romaniello <37159767+luccaromaniello@users.noreply.github.com>
2025-12-14 22:33:01 -0300

    style: header back button

--- src/components/IconButton.tsx
+++ src/components/IconButton.tsx
@@ -5,22 +5,38 @@ import { HiBellAlert } from "react-icons/hi2";
 
 interface IconButtonProps {
   onClick?: () => void;
-  icon?: IconType;
+  icon: {
+    type: IconType;
+    background?: boolean;
+    size?: number;
+  };
   link?: string;
 }
 
 const IconButton: React.FC<IconButtonProps> = ({
   onClick,
-  icon = HiBellAlert,
+  icon = { type: HiBellAlert, background: true, size: 6 },
   link,
 }) => {
-  const IconComponent = icon || HiBellAlert;
-  const buttonStyle =
-    "flex items-center rounded-lg text-primary text-lg cursor-pointer hover:text-secondary";
+  const IconComponent = icon.type || HiBellAlert;
+  const buttonSizes: Record<number, string> = {
+    4: "w-4 h-4",
+    6: "w-6 h-6",
+    8: "w-8 h-8",
+    10: "w-10 h-10",
+  };
+  const buttonStyle = `flex items-center text-lg cursor-pointer rounded-full justify-center transition-colors
+    ${
+      icon.background
+        ? "bg-gray-200 text-neutral-black p-1 hover:bg-gray-300"
+        : "bg-transparent text-primary hover:text-primary/80"
+    }`;
 
   const buttonContent = (
     <>
-      <IconComponent className="h-6 w-6" />
+      <IconComponent
+        className={icon.size ? buttonSizes[icon.size] : "w-6 h-6"}
+      />
     </>
   );
 
--- src/styles/constants.ts
+++ src/styles/constants.ts
@@ -2,4 +2,4 @@ export const PADDING_X = "px-4";
 export const PADDING_Y = "py-8";
 export const BORDER_BOTTOM = "border-b-[0.5px] border-gray-300";
 export const HEADER_CONTAINER_PRIMARY = `flex flex-row items-center justify-between ${BORDER_BOTTOM} ${PADDING_X} py-3 h-16`;
-export const HEADER_CONTAINER_SECONDARY = `flex flex-row items-center ${BORDER_BOTTOM} ${PADDING_X} py-3 h-16`;
+export const HEADER_CONTAINER_SECONDARY = `relative flex flex-row items-center justify-center ${BORDER_BOTTOM} ${PADDING_X} py-3 h-16`;
