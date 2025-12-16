8ab4a12b43b7fbd2d0a93581a10b292e2330a053
Lucca Romaniello <37159767+luccaromaniello@users.noreply.github.com>
2025-12-15 23:27:17 -0300

    feat: updates input and button of global hotkeys

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,5 @@
 export * from "./settings";
+export { default as Button } from "./Button";
 export { default as IconButton } from "./IconButton";
+export { default as Input } from "./Input";
 export { default as Header } from "./Header";
---
+++ src/i18n/locales/components/index.ts
@@ -0,0 +1,5 @@
+import enJson from "./en.json";
+import ptJson from "./pt.json";
+
+export const en = { components: enJson };
+export const pt = { components: ptJson };
