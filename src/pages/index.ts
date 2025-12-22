b73b9cb6549fb339210921cf891ef9754b8b1134
Lucca Romaniello <37159767+luccaromaniello@users.noreply.github.com>
2025-12-22 18:34:40 -0300

    refactor: updates settings home to be an index file

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,5 +1,5 @@
 export { default as Root } from "./Root";
 export { default as Home } from "./Home";
-export { default as Settings } from "./Settings";
+export { default as Settings } from "./settings";
 
 export { default as Hotkeys } from "./settings/Hotkeys";
