f464239a676ad22a9765672f08762718cbcda096
Lucca Romaniello <37159767+luccaromaniello@users.noreply.github.com>
2026-01-19 10:27:36 -0300

    fix: clear clipboard history on mac

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -38,6 +38,7 @@ pub fn run() {
         .plugin(tauri_plugin_positioner::init())
         .plugin(tauri_plugin_shell::init())
         .plugin(tauri_plugin_opener::init())
+        .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_global_shortcut::Builder::new().build())
         .invoke_handler(tauri::generate_handler![
             items::get_items,
