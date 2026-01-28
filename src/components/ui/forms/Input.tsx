d5954f90a372b4583e601ab7875b9e59bb8b73f8
Lucca Romaniello <37159767+luccaromaniello@users.noreply.github.com>
2026-01-28 12:56:31 -0300

    style: updates caret input color

--- src/components/ui/forms/Input.tsx
+++ src/components/ui/forms/Input.tsx
@@ -39,7 +39,7 @@ const Input = forwardRef<HTMLInputElement, InputProps>(
           readOnly={readOnly}
           placeholder={placeholder}
           className={`${rounded} w-full min-w-0 px-3 py-2 border border-neutral-300 dark:border-neutral-600 text-sm h-10 focus:outline-none focus:ring-2 focus:ring-secondary focus:border-transparent
-            ${readOnly ? "bg-neutral-200 dark:bg-neutral-black/10 text-neutral-600 dark:text-neutral-400 cursor-not-allowed" : "bg-white dark:bg-neutral-black/10 dark:text-neutral-white"}
+            ${readOnly ? "bg-neutral-200 dark:bg-neutral-black/10 text-neutral-600 dark:text-neutral-400 cursor-not-allowed" : "bg-white dark:bg-neutral-black/10 dark:text-neutral-white caret-secondary"}
             ${attachedToButton ? "rounded-r-none" : ""}`}
         />
         {helperText && <HelperText text={helperText} />}
